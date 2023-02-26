b77718e2fe2f01212a30282a0ea727cd2c082c2c
CodaCodalis <larah.wuxus@gmail.com>
2023-02-26 11:11:55 +0100

    initial

---
+++ main.py
@@ -0,0 +1,51 @@
+import random
+
+
+# Create a function called menu that prints the following menu:
+# 1. Guess a number
+# 2. Exit
+def menu():
+    print("1. Guess a number")
+    print("2. Exit")
+
+
+# Create a function called guess_a_number
+def guess_a_number():
+    # Create a list of 100 numbers
+    numbers = list(range(1, 101))
+    # Ask the user to enter a number between 1 and 100
+    user_number = int(input("Enter a number between 1 and 100: "))
+    # randomly pick a number from the list
+    random_number = random.choice(numbers)
+    # compare the user's number to the random number
+    # if the user's number is the same as the random number, print "You guessed the number!"
+    if user_number == random_number:
+        print("You guessed the number!")
+    # if the user's number is higher than the random number, print "You guessed too high!"
+    elif user_number > random_number:
+        print("You guessed too high!")
+    # if the user's number is lower than the random number, print "You guessed too low!"
+    elif user_number < random_number:
+        print("You guessed too low!")
+    # print the random number
+    print("Actual number: " + str(random_number))
+    # print the user's number
+    print("Your number: " + str(user_number))
+
+
+if __name__ == '__main__':
+    # Create a while loop that runs until the user enters 2
+    while True:
+        # Call the menu function
+        menu()
+        # Ask the user to enter a number
+        user_input = int(input("Enter a number: "))
+        # if the user enters 1, call the guess_a_number function
+        if user_input == 1:
+            guess_a_number()
+        # if the user enters 2, exit the program
+        elif user_input == 2:
+            break
+        # if the user enters anything else, print "Invalid input"
+        else:
+            print("Invalid input")
